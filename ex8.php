2b77a362273768be2aefe70b240987fb7512ea5b
kiks27 <80412065+kiks27@users.noreply.github.com>
2021-11-16 17:25:01 +0100

    commit2

---
+++ ex8.php
@@ -0,0 +1,33 @@
+<html>
+
+<head><title>Exercice 8</title></head>
+
+<body>
+
+<form method="POST">
+    <input type="text" id="name" name="Nom">
+    <input type="text" id="name" name="Prénom">
+    <input type="submit" id="value" value="Cliquer">
+    <input type="submit" id="value" value="Reset">
+  
+    <?php   
+
+    session_start();
+    if (isset($_POST['Nom'])) {
+        echo "Votre nom est : " .$_POST['Nom'];
+    }
+
+    ?>
+
+    <?php
+    if (isset($_POST['Prénom'])) {
+        echo "Votre prénom est : " .$_POST['Prénom'];
+    }
+
+    ?>
+
+    <p><a href="/php/huet/">Accueil</a></p>
+
+</body> 
+
+</html>
